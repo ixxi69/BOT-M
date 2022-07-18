c5b3699c7e4915f1e80c8fa27b0e75d49737c536
ixxi69 <109481526+ixxi69@users.noreply.github.com>
2022-07-18 13:47:57 -0600

    ixxi

--- handler.js
+++ handler.js
@@ -614,8 +614,8 @@ export async function handler(chatUpdate) {
         if (opts['autoread'])
             await this.readMessages([m.key])
         
-        if (!m.fromMem && m.text.match(/(ixxi|@51959719359|bot|mystic|the mystic - bot|mystic - bot|themystic-bot)/gi)) {
-        let emot = pickRandom(["🎃", "❤", "😘", "😍", "💕", "😎", "🙌", "⭐", "👻", "🔥"])
+        if (!m.fromMem && m.text.match(/(ixxi|@51959719359|bot|issi|nanailnymosis|feo)/gi)) {
+        let emot = pickRandom(["⚘", "❤", "📸", "😍", "💕", "💣", "🫂", "👅", "👻", "🔥"])
         this.sendMessage(m.chat, { react: { text: emot, key: m.key }})}
         function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]}
     }
