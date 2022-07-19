12b5aaac50cbe5abcf6fc11a20930b56c4feb675
ixxi69 <109481526+ixxi69@users.noreply.github.com>
2022-07-19 12:27:58 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -77,9 +77,7 @@ ________________________
 ❏ kxrlos oɟɟ.
 
 ʍǝnu x nʎɯos oɟɟ.
-_______________
 `.trim()
-conn.sendHydrated2(m.chat, str, wm, pp, 'chupaditas de tetas al dm de ixxi', 'Sexo', 'https://github.com/ixxi69/Bot-M', '𝙶𝙸𝚃𝙷𝚄𝙱', [
 ['MENU', '/Menu3'],
 ], m,)
 //await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {
