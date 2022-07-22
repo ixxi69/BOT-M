930e002dabf589ffa7c7df456d27fab93700b90b
ixxi69 <109481526+ixxi69@users.noreply.github.com>
2022-07-21 18:40:33 -0600

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -87,7 +87,7 @@ conn.sendHydrated2(m.chat, str, wm, pp, 'SEXO ANAL AL PV', 'SEXO', 'https://gith
 conn.reply(m.chat, '*[𝐈𝐍𝐅𝐎] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
 throw e
 }}
-handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos)$/i
+handler.command = (menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos)$/i
 handler.exp = 50
 handler.fail = null
 export default handler
